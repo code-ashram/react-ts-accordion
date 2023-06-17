27773f2f986a700b2520f728741f73408496fc00
Andrei Yeutukhou <gavnorock84@hotmail.com>
2023-06-17 21:15:35 +0300

    add autoclosing for items

--- src/Components/List/List.tsx
+++ src/Components/List/List.tsx
@@ -1,14 +1,26 @@
 import data from "../../data.ts";
-import ListItem from "../ListItem";
+import {FC, useState} from "react";
+// import ListItem from "../ListItem";
 
-const List = () => (
-    <div className='accordion'>
-        <ul className="accordion__list p-4 text-amber-200 border-2 border-amber-50 rounded-md">
-            {data.map((listItem) =>
-                <ListItem key={listItem.id} title={listItem.title} text={listItem.text}/>
-            )}
-        </ul>
-    </div>
-)
+const List = () => {
+
+    const [activeIndex, setActiveIndex] = useState(-1);
+    const handleClick = (index: number) => {
+        setActiveIndex(index === activeIndex ? -1 : index);
+    };
+
+    return (
+        <div className='accordion'>
+            <ul className="accordion__list p-4 text-amber-200 border-2 border-amber-50 rounded-md">
+                {data.map((listItem, index) =>
+                    <li onClick={() => handleClick(index)} className="accordion__item p-2 border-2 border-amber-500 rounded">
+                        <h2 className="item__title text-center text-xl font-bold hover:text-sky-200">{listItem.title}</h2>
+                        {index === activeIndex && <p className="item__text">{listItem.text}</p>}
+                    </li>
+                )}
+            </ul>
+        </div>
+    )
+}
 
 export default List
--- src/data.ts
+++ src/data.ts
@@ -2,6 +2,7 @@ type Shloka = {
     id: string,
     title: string,
     text: string,
+    isOpen: boolean,
 }
 
 const data: Array<Shloka> = [
@@ -10,19 +11,22 @@ const data: Array<Shloka> = [
         title: 'Text 1',
         text: 'The Supreme Personality of Godhead said: Listen again, O mighty-armed Arjuna. Because you are ' +
             'My dear friend, for your benefit I shall speak to you further, giving knowledge that is better than ' +
-            'what I have already explained.'
+            'what I have already explained.',
+        isOpen: false,
     },
     {
         id: 'c10t2',
         title: 'Text 2',
         text: 'Neither the hosts of demigods nor the great sages know My origin or opulences, for, in every respect,' +
-            ' I am the source of the demigods and sages.'
+            ' I am the source of the demigods and sages.',
+        isOpen: false,
     },
     {
         id: 'c10t3',
         title: 'Text 3',
         text: 'He who knows Me as the unborn, as the beginningless, as the Supreme Lord of all the worlds – he only,' +
-            ' undeluded among men, is freed from all sins.'
+            ' undeluded among men, is freed from all sins.',
+        isOpen: false,
     },
     {
         id: 'c10t4-5',
@@ -30,14 +34,16 @@ const data: Array<Shloka> = [
         text: 'Intelligence, knowledge, freedom from doubt and delusion, forgiveness, truthfulness, control of ' +
             'the senses, control of the mind, happiness and distress, birth, death, fear, fearlessness, nonviolence,' +
             ' equanimity, satisfaction, austerity, charity, fame and infamy – all these various qualities of living' +
-            ' beings are created by Me alone.'
+            ' beings are created by Me alone.',
+        isOpen: false,
     },
     {
         id: 'c10t6',
         title: 'Text 5',
         text: 'The seven great sages and before them the four other great sages and the Manus [progenitors of ' +
             'mankind] come from Me, born from My mind, and all the living beings populating the various planets ' +
-            'descend from them.'
+            'descend from them.',
+        isOpen: false,
     },
 ]
 
